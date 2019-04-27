4798778c72231fbe37377dcce383317cf434d5d2
Seb Jones <jones.seb95@googlemail.com>
2019-04-27 19:17:18 +0100

    Remove unused keys.

--- globals.c
+++ globals.c
@@ -1,10 +1,6 @@
 // System
 double elapsed_seconds;
 
-const char *right_key = "Right";
-const char *left_key = "Left";
-const char *up_key = "Up";
-const char *down_key = "Down";
 const char *a_key = "A";
 const char *b_key = "B";
 const char *c_key = "C";
@@ -18,5 +14,5 @@ int     day = 0;
 int     hour = 12;
 int     minute = 58;
 int     food = 1;
-int     bandages = 1;
+int     bandages = 0;
 int     money = 5;
